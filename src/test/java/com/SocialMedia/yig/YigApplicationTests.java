1be32111e5b8f9af0c2fd8bde66b307ab9ce0501
yigit <myigitsyn@gmail.com>
2025-04-04 00:47:59 +0300

    Social Media

---
+++ src/test/java/com/SocialMedia/yig/YigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SocialMedia.yig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
